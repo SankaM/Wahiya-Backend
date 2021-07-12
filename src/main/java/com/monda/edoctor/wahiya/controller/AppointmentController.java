e1440e9dcd3724cfdbf730dcd5370cc3beea2e6e
desdrianton <desdrianton@gmail.com>
2021-07-12 09:21:22 +0700

    Create empty controller

---
+++ src/main/java/com/monda/edoctor/wahiya/controller/AppointmentController.java
@@ -0,0 +1,13 @@
+package com.monda.edoctor.wahiya.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/v1")
+public class AppointmentController {
+}

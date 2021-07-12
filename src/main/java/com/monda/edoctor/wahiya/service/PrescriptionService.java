1d395fa843e50c399a3a3cb54e009bffc122de05
desdrianton <desdrianton@gmail.com>
2021-07-12 09:17:31 +0700

    Create empty service

---
+++ src/main/java/com/monda/edoctor/wahiya/service/PrescriptionService.java
@@ -0,0 +1,9 @@
+package com.monda.edoctor.wahiya.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PrescriptionService {
+}

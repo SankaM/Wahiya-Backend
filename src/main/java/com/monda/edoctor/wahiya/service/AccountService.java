9abe94cb0072056144e5cefbd4761e9b9bbaa10d
desdrianton <desdrianton@gmail.com>
2021-08-05 09:14:32 +0700

    Update profile

--- src/main/java/com/monda/edoctor/wahiya/service/AccountService.java
+++ src/main/java/com/monda/edoctor/wahiya/service/AccountService.java
@@ -1,7 +1,10 @@
 package com.monda.edoctor.wahiya.service;
 
 import com.monda.edoctor.wahiya.dto.req.LoginReq;
+import com.monda.edoctor.wahiya.dto.req.UpdatePasswordReq;
 import com.monda.edoctor.wahiya.exception.LoginException;
+import com.monda.edoctor.wahiya.exception.NotFoundException;
+import com.monda.edoctor.wahiya.exception.WrongParameterException;
 import com.monda.edoctor.wahiya.model.DoctorEntity;
 import com.monda.edoctor.wahiya.repository.DoctorRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -9,11 +12,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Slf4j
 @Service
-public class LoginService {
+public class AccountService {
     @Autowired
     private DoctorRepository doctorRepository;
+
+    @Autowired
+    private DoctorService doctorService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -26,5 +34,17 @@ public class LoginService {
 
         log.debug("Password does not matched :" + doctorEntity.getUserName());
         throw new LoginException();
+    }
+
+    public void updatePassword(UUID doctorId, UpdatePasswordReq req) throws NotFoundException, WrongParameterException {
+        doctorService.existsById(doctorId);
+
+        DoctorEntity doctor = doctorRepository.getOne(doctorId);
+        if (!passwordEncoder.matches(req.getOldPassword(), doctor.getPassword())) {
+            throw new WrongParameterException("Wrong old password");
+        }
+
+        doctor.setPassword(passwordEncoder.encode(req.getNewPassword()));
+        doctorRepository.save(doctor);
     }
 }
